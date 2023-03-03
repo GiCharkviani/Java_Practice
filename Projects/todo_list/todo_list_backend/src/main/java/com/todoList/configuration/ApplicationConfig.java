03fb500db09aeab68b6234b45ddd031716c2ba50
GiCharkviani <gcharkvi@broadinstitute.org>
2023-03-03 18:26:24 +0400

    ss

--- Projects/todo_list/todo_list_backend/src/main/java/com/todoList/configuration/ApplicationConfig.java
+++ Projects/todo_list/todo_list_backend/src/main/java/com/todoList/configuration/ApplicationConfig.java
@@ -24,6 +24,7 @@ import java.util.List;
 /**
  * @TODO
  * 1. Check transaction join and bulk delete
+ * 2. Investigate further error responses
  * 10. add email and other checks, that there is used right format
  * 5. Handle all possible exceptions and send appoperate response
  * 6. Refactor code a bit
